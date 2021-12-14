f269eecf7ec14c08b39fd5bcaf81370d5411b45f
ilir2523 <ilirrukaj25@gmail.com>
2021-12-14 16:00:51 +0100

    add the item to the bag the first time
    increase the quantity of this item in the bag subsequent times
    
    3 links to filter pages: Girls, Guys, Sale

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const state = {
     store: [],
-    tab: '',
+    itemType: ['Girls', 'Guys', 'Sale'],
+    tab: null,
     modal: '',
     search: '',
     user: null,
@@ -12,34 +13,78 @@ function fetchStore() {
     return fetch("http://localhost:3000/store").then(resp => resp.json())
 }
 
-function addItemToStore() {
-    fetchStore().then(function(item) {
-        state.store = item
-        render()
-    })
+function watchNumberOfItem() {
+    return state.bag.length
 }
 
-addItemToStore()
+function removeItemFromBag() {
+    
+}
 
 function renderHeader() {
     const headerEl =  document.createElement('header')
 
     const h1El = document.createElement('h1')
     h1El.textContent = 'Hollixton'
+    h1El.addEventListener('click', function() {
+        state.selectedItem = null
+        state.tab = 'Home'
+        watchTabAndAddItemToStore()
+        render()
+    })
 
     const navUlEl = document.createElement('ul')
-    navUlEl.innerHTML = `
-    <li><a href="">Girls</a></li>
-    <li><a href="">Guys</a></li>
-    <li><a href="">Sale</a></li>
-    `
+
+    for (type of state.itemType) {
+        const typeLiEl = document.createElement('li')
+
+        const typeLinkEl = document.createElement('a')
+        typeLinkEl.textContent = type
+        let selectedType = type
+        typeLinkEl.addEventListener('click', function() {
+            state.selectedItem = null
+            state.tab = selectedType
+            console.log(selectedType)
+            watchTabAndAddItemToStore()
+            render()
+        })
+
+        typeLiEl.append(typeLinkEl)
+        navUlEl.append(typeLiEl)
+    }
 
     const navIconUlEl = document.createElement('ul')
-    navIconUlEl.innerHTML = `
-    <li><a href="">Search</a></li>
-    <li><a href="">Sign-in</a></li>
-    <li><a href="">Bag</a></li>
-    `
+    const searchLiEl = document.createElement('li')
+    const searchButtonEl = document.createElement('button')
+    const searchImgEl = document.createElement('img')
+    searchImgEl.setAttribute('src', 'icons/search_black_24dp.svg')
+
+    searchButtonEl.append(searchImgEl)
+    searchLiEl.append(searchButtonEl)
+
+    const personLiEl = document.createElement('li')
+    const personButtonEl = document.createElement('button')
+    const personImgEl = document.createElement('img')
+    personImgEl.setAttribute('src', 'icons/person_black_24dp.svg')
+
+    personButtonEl.append(personImgEl)
+    personLiEl.append(personButtonEl)
+
+    const shoppingLiEl = document.createElement('li')
+    const shoppingButtonEl = document.createElement('button')
+    shoppingButtonEl.setAttribute('class', 'shopping-list')
+    const shoppingImgEl = document.createElement('img')
+    shoppingImgEl.setAttribute('src', 'icons/shopping_bag_black_24dp.svg')
+
+    const itemInShopingEl = document.createElement('span')
+    itemInShopingEl.setAttribute('class', 'item-in-shopping-list')
+    itemInShopingEl.textContent = watchNumberOfItem()
+
+    shoppingButtonEl.append(shoppingImgEl, itemInShopingEl)
+    shoppingLiEl.append(shoppingButtonEl)
+
+
+    navIconUlEl.append(searchLiEl, personLiEl, shoppingLiEl)
 
     headerEl.append(h1El, navUlEl, navIconUlEl)
 
@@ -50,7 +95,7 @@ function renderMain() {
     const mainEl = document.createElement('main')
 
     const h2El = document.createElement('h2')
-    h2El.textContent = 'Home'
+    h2El.textContent = state.tab
 
     const mainUlEl = document.createElement('ul')
     mainUlEl.setAttribute('class', 'list-secction')
@@ -60,6 +105,12 @@ function renderMain() {
 
         const itemLiEl = document.createElement('li')
         itemLiEl.setAttribute('class', 'item-list-secction')
+
+        itemLiEl.addEventListener('click', function(){
+            state.selectedItem = item.id
+            // console.log(item.id)
+            render()
+        })
 
         const itemImageEl = document.createElement('img')
         itemImageEl.setAttribute('class', 'image-list-secction')
@@ -72,7 +123,19 @@ function renderMain() {
 
         const itemPriceEl = document.createElement('p')
         itemPriceEl.setAttribute('class', 'price-list-secction')
-        itemPriceEl.textContent = `£${item.price}`
+        if (item.discountedPrice === undefined) {
+            itemPriceEl.textContent = `£${item.price}`
+        } else {
+            itemPriceEl.innerHTML = `<span style="text-decoration-line: line-through;">£${item.price}</span> <span style="color: red;">£${item.discountedPrice}</span>`
+        }
+
+        if (item.dateEntered[6] >= 8) {
+            const newEl = document.createElement('span')
+            newEl.setAttribute('class', 'new-item')
+            newEl.textContent = 'NEW!'
+
+            itemLiEl.append(newEl)
+        }
 
         itemLiEl.append(itemImageEl, itemNameEl, itemPriceEl)
 
@@ -102,13 +165,90 @@ function renderFooter() {
 }
 
 
+function renderOneItem(items) {
+    // console.log(items)
+    let item = null
+    for (const myitem of items){
+        if (myitem.id === state.selectedItem){
+            item = myitem
+        }
+    }
+    const mainEl = document.createElement('main')
+    mainEl.setAttribute('class', 'one-item-main')
+
+    const itemImageEl = document.createElement('img')
+        itemImageEl.setAttribute('class', 'one-item-image')
+        itemImageEl.setAttribute('src', item.image)
+        itemImageEl.setAttribute('alt', 'one item image')
+
+    const oneItemSeccionEl = document.createElement('secction')
+    oneItemSeccionEl.setAttribute('class', 'one-item-secction')
+
+    const itemNameEl = document.createElement('h3')
+    itemNameEl.setAttribute('class', 'name-list-secction')
+    itemNameEl.textContent = item.name.toUpperCase()
+
+    const buttonEl = document.createElement('button')
+    buttonEl.setAttribute('class', 'one-item-button')
+    buttonEl.textContent = 'ADD TO BAG'
+
+    buttonEl.addEventListener('click', function() {
+        state.bag.push(item.name)
+        render()
+    })
+
+    oneItemSeccionEl.append(itemNameEl, buttonEl)
+    
+    mainEl.append(itemImageEl, oneItemSeccionEl)
+
+    document.body.append(mainEl)
+
+}
 
 function render() {
     document.body.innerHTML = ''
 
     renderHeader()
-    renderMain()
+    if (state.selectedItem === null){
+        renderMain()
+    } else {
+        renderOneItem(state.store)
+    }
+    
     renderFooter()
 }
+
+function addItemToStore() {
+    fetchStore().then(function(items) {
+        state.store = items
+        render()
+    })
+}
+
+function addGirlsItemToStore() {
+    fetchStore().then(function(items) {
+        state.store = items.filter(item => item.type === "Girls")
+        render()
+    })
+}
+
+function addGuysItemToStore() {
+    fetchStore().then(function(items) {
+        state.store = items.filter(item => item.type === "Guys")
+        render()
+    })
+}
+
+function watchTabAndAddItemToStore() {
+    if (state.tab === "Girls") {
+        addGirlsItemToStore()
+    } else if (state.tab === "Guys") {
+        addGuysItemToStore()
+    } else if (state.tab === null || "Sale" ||"Home") {
+        addItemToStore()
+    }
+}
+
+watchTabAndAddItemToStore()
 
 render()
